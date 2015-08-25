991fab6e9488c3f05ecd48150ec7f9dfd9d928a4
afg <afg984@gmail.com>
2015-08-25 23:49:37 +0800

    updated navdata decoder

--- pyardrone/navdata/__init__.py
+++ pyardrone/navdata/__init__.py
@@ -1,19 +1,43 @@
 import struct
+import io
+import reprlib
 
 
 header = struct.pack('<i', 0x55667788)
 
 
-class NavOption:
-
-    def __init__(self, ib):
-        self.tag, self.size = struct.unpack('<hh')
-        self.data = struct.unpack('<' + 'b' * self.size)
-
-
 class NavData:
 
-    def __init__(self, ib):
+    def __init__(self, bytes_):
+        flo = io.BytesIO(bytes_)
         (
-            self.drone_state, self.seq_num, self.vision_flag
-        ) = struct.unpack('<iii', ib)
+            self.header,
+            self.drone_state,
+            self.sequence_number,
+            self.version_flag
+        ) = struct.unpack('<IIII', flo.read(16))
+        self.options = []
+        while flo.tell() != len(bytes_):
+            self.options.append(NavOption(flo))
+
+    def __repr__(self):
+        return (
+            '{self.__class__.__name__}'
+            '(drone_state={self.drone_state}, '
+            'sequence_number={self.sequence_number}, '
+            'version_flag={self.version_flag}, options={options})').format(
+            self=self,
+            options=reprlib.repr(self.options)
+        )
+
+
+class NavOption:
+
+    def __init__(self, flo):
+        self.id, self.size = struct.unpack('<HH', flo.read(4))
+        self.data = flo.read(self.size)
+
+    def __repr__(self):
+        return (
+            '{self.__class__.__name__}'
+            '(id={self.id}, size={self.size})').format(self=self)
