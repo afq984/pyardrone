d3e27b5f3be782a9081c00567c22e50eba3cdb7a
afg <afg984@gmail.com>
2015-08-27 00:16:05 +0800

    use threads to send commands

--- pyardrone/__init__.py
+++ pyardrone/__init__.py
@@ -2,9 +2,13 @@
 High level ARDrone API
 '''
 
-import socket
-import logging
+import collections
 import itertools
+import logging
+import queue
+import socket
+import threading
+import time
 
 from pyardrone.config import Config
 from pyardrone import at
@@ -12,6 +16,9 @@ from pyardrone.utils import noop
 
 
 logger = logging.getLogger(__name__)
+
+
+QueuedCommand = collections.namedtuple('QueuedCommand', 'command event')
 
 
 class ARDrone:
@@ -33,11 +40,16 @@ class ARDrone:
         self.control_port = control_port
         self.interval = interval
 
+        self.init_sockets()
+
         # sequence number required by ATCommands
         # DevGuide: send 1 as the sequence number of the first sent command
         self.seq_num = 0
 
-        self.init_sockets()
+        self.seq_lock = threading.Lock()
+        self.queued_commands = queue.Queue()
+        self._at_thread = threading.Thread(target=self._at_job)
+        self._at_stop = threading.Event()
 
         self.config = Config(self)
 
@@ -57,20 +69,39 @@ class ARDrone:
         self.navdata_sock.close()
         self.control_sock.close()
 
+    def register(self, command, *, event=None):
+        self.queued_commands.put_nowait(QueuedCommand(command, event))
+        return event
+
     def send(self, command):
-        self.seq_num += 1
-        packed = command.pack(self.seq_num)
-        bytes_sent = self.at_sock.sendto(packed, (self.addr, self.at_port))
-        logger.debug(
-            'sent %d bytes to "%s:%d", %r',
-            bytes_sent,
-            self.addr,
-            self.at_port,
-            packed
-        )
+        self.register(command, event=threading.Event()).wait()
+
+    def send_nowait(self, command):
+        with self.seq_lock:
+            self.seq_num += 1
+            packed = command.pack(self.seq_num)
+            bytes_sent = self.at_sock.sendto(packed, (self.addr, self.at_port))
+            logger.debug(
+                'sent %d bytes to "%s:%d", %r',
+                bytes_sent,
+                self.addr,
+                self.at_port,
+                packed
+            )
 
     def get_raw_config(self):
         self.send(at.CTRL(at.CTRL.Modes.CFG_GET_CONTROL_MODE))
         return b''.join(
             itertools.dropwhile(noop, self.control_sock.recv(4096))
         ).encode()
+
+    def _at_job(self):
+        while not self._at_stop.isSet():
+            try:
+                qcmd = self.queued_commands.get_nowait()
+            except queue.Empty:
+                qcmd = QueuedCommand(at.COMWDG(), event=None)
+            self.send_nowait(qcmd.command)
+            if qcmd.event is not None:
+                qcmd.event.set()
+            time.sleep(self.interval)
