db31e1dd82353cd1fc7a9ed7186c906c860ef42d
afg <afg984@gmail.com>
2015-09-06 18:13:37 +0800

    remove object_executor related tests

--- tests/test_ardrone.py
+++ tests/test_ardrone.py
@@ -2,21 +2,6 @@ import unittest
 import pyardrone
 
 
-class ARDroneIntervalTest(unittest.TestCase):
-
-    def setUp(self):
-        self.drone = pyardrone.ARDrone(connect=False)
-
-    def testSetInterval(self):
-        self.drone.interval = 200
-        self.assertEqual(self.drone.interval, 200)
-        self.assertEqual(self.drone._at_executor.interval, 200)
-
-    def testSetExecutorInterval(self):
-        self.drone._at_executor.interval = 30
-        self.assertEqual(self.drone.interval, 30)
-
-
 class ARDroneConnectExceptionTest(unittest.TestCase):
 
     def setUp(self):
