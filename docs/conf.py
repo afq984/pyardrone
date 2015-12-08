f5dc737eb9f5aa6f4b37a7d7d23b1747f7ab89ec
afg <afg984@gmail.com>
2015-12-09 06:50:40 +0800

    dummy cv2 module for documentation

--- docs/conf.py
+++ docs/conf.py
@@ -17,6 +17,13 @@ import sys
 import os
 import shlex
 
+try:
+    import cv2
+except ImportError:
+    import types
+    sys.modules['cv2'] = types.ModuleType('cv2')
+    import cv2
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
